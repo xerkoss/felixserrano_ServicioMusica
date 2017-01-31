8c0859c8bcce8319eb0441ac22f6320556fc5d2f
xerkoss <xerkossdev@gmail.com>
2017-01-31 16:16:25 +0100

    ejercicio 8.3

--- app/src/main/java/serranofelix/com/serviciomsica/ServicioMusica.java
+++ app/src/main/java/serranofelix/com/serviciomsica/ServicioMusica.java
@@ -1,5 +1,8 @@
 package serranofelix.com.serviciomsica;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -8,21 +11,40 @@ import android.widget.Toast;
 
 
 public class ServicioMusica extends Service {
+    private NotificationManager notificationManager;
+    private static final int ID_NOTIFICACION = 1;
+
     MediaPlayer reproductor;
     @Override
     public void onCreate() {
         Toast.makeText(this, "Servicio creado",
                 Toast.LENGTH_SHORT).show();
         reproductor = MediaPlayer.create(this, R.raw.audio);
+        notificationManager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
     }
-    /*@Override
-    public int onStartCommand(Intent intent, int flags,
-                              int idArranque) {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int idArranque) {
         Toast.makeText(this,"Servicio arrancado "+ idArranque,
                 Toast.LENGTH_SHORT).show();
         reproductor.start();
+        PendingIntent pendingIntent = PendingIntent
+                .getActivity(this, 0, intent, 0);
+        long vibrate[] = {0, 100, 200, 300};
+        @SuppressWarnings("deprecation")
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("Reproduciendo música")
+                .setContentText("Información adicional")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(pendingIntent)
+                .setVibrate(vibrate)
+                .setLights(0xffff00ff,0,0)
+                .getNotification();
+        notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+        notificationManager.notify(ID_NOTIFICACION, notification);
+
         return START_STICKY;
-    }*/
+    }
 
     @SuppressWarnings("deprecation")
     @Override
@@ -37,6 +59,7 @@ public class ServicioMusica extends Service {
         Toast.makeText(this,"Servicio detenido",
                 Toast.LENGTH_SHORT).show();
         reproductor.stop();
+        notificationManager.cancel(ID_NOTIFICACION);
     }
     @Override
     public IBinder onBind(Intent intent) {
